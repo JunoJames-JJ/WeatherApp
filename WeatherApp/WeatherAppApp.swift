e8cb308d2aa1117f83fdfd8c2d2a22f6c72bb55a
junojobin <junojamesv@gmail.com>
2024-10-28 14:57:19 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Juno James on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
