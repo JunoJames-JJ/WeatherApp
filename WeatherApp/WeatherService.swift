ae36f3021f478166e77f21f2ced66559802d680f
junojobin <junojamesv@gmail.com>
2024-10-28 15:09:18 -0500

    Implement WeatherService for API call

---
+++ WeatherApp/WeatherService.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Jobin Joseph on 2024-10-28.
+//
+
+import Foundation
+import Combine
+
+protocol WeatherServiceProtocol {
+    func fetchWeather(latitude: Double, longitude: Double) -> AnyPublisher<Weather, Error>
+}
+
+class WeatherService: WeatherServiceProtocol {
+    private let apiKey = "0d6bc7639dacbd17179631b3e1236c14" // Add API Key in here..
+    
+    func fetchWeather(latitude: Double, longitude: Double) -> AnyPublisher<Weather, Error> {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)")
+        else { return Fail(error: URLError(.badURL)).eraseToAnyPublisher() }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: Weather.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
+
